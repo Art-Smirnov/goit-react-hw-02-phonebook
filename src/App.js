3ac02e713fe6f31849438d053e741f805657827b
Artem Smirnov <aratatem2013@gmail>
2021-06-06 11:27:21 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import initialContacts from './data/contacts.json';
+
 import { v4 as uuidv4 } from 'uuid';
 import ContactForm from './Components/ContactForm';
 import ContactList from './Components/ContactList';
@@ -8,13 +10,7 @@ import Container from './Components/Container';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-
+    contacts: initialContacts,
     filter: '',
   };
 
